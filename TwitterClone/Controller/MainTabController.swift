25c685d0edc5c6abb3b3e74cb856e1e9344eb311
Chirag Arora <arora2002chirag@gmail.com>
2023-02-16 00:28:24 +0530

    Setup Root ViewController

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterClone
 //
 //  Created by chirag arora on 16/02/23.
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPink
     }
-
+    
+    
+    
+ 
 
 }
-
