1b6d9c62472272c422b37ec69b7da6eae1e8483e
Chirag Arora <arora2002chirag@gmail.com>
2023-10-27 01:51:52 +0530

    Login/Signup Page

---
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -0,0 +1,125 @@
+//
+//  LoginController.swift
+//  TwitterClone
+//
+//  Created by chirag arora on 26/10/23.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = UIImage(imageLiteralResourceName: "TwitterLogo")
+        
+        return iv
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        let image = UIImage(imageLiteralResourceName: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+        
+        
+        return view
+        
+        
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let image = UIImage(imageLiteralResourceName: "ic_lock_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+
+        
+        return view
+        
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceHolder: "Email")
+        return tf
+        
+    }()
+    
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceHolder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+        
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.TwitterBlue, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = Utilities().attributedButton("Don't have an account?", " Sign Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    
+    
+    // MARK: - Selectors
+    
+    @objc func handleShowSignUp() {
+        let controller = RegistrationController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @objc func handleLogin() {
+        print("Handle Login here..")
+    }
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .TwitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fillEqually
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingleft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingleft: 40, paddingRight: 40)
+        
+        
+        
+    }
+    
+    
+    
+    
+}
--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -11,15 +11,38 @@ class MainTabController: UITabBarController {
     
     // MARK: - Properties
     
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .TwitterBlue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.addTarget(MainTabController.self, action: #selector(actionButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureViewControllers()
+        configureUI()
     }
     
-    //MARK: - Helpers
+    // MARK: - Selectors
+    
+    @objc func actionButtonTapped() {
+        print(123)
+    }
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.addSubview(actionButton)
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.layer.cornerRadius = 56/2
+    }
     
     func configureViewControllers() {
         
@@ -37,7 +60,7 @@ class MainTabController: UITabBarController {
         let nav4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootVieweController: conversations)
         
         
-        viewControllers = [nav1,nav2,nav3,nav4]
+        viewControllers = [nav1, nav2, nav3, nav4]
         
     }
     
