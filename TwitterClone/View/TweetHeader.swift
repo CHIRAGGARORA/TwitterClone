2c3d13e0b264d5ea2821b80e64b8d2f3e2fdb400
Chirag Arora <arora2002chirag@gmail.com>
2023-12-29 01:04:04 +0530

    Tweet Display Controller Setup

---
+++ TwitterClone/View/TweetHeader.swift
@@ -0,0 +1,26 @@
+//
+//  TweetHeader.swift
+//  TwitterClone
+//
+//  Created by chirag arora on 29/12/23.
+//
+
+import UIKit
+
+class TweetHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
